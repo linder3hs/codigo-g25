5475ad0259ee343981a811feaea0ed0e1368f0b2
linder3hs <linderhassinger00@gmail.com>
2025-05-01 22:39:31 -0500

    add task

--- semana-4/clase-2/proyecto/app.js
+++ semana-4/clase-2/proyecto/app.js
@@ -1,5 +1,7 @@
 const form = document.querySelector("form");
+
 const taskCount = document.getElementById("task-count");
+const taskList = document.querySelector("#tasks-list");
 
 const notyf = new Notyf({
   position: {
@@ -30,12 +32,14 @@ form.addEventListener("submit", function (event) {
 
   const formData = new FormData(this);
   const task = formData.get("task");
-
-  tasks.push({
+  const newTask = {
     task,
     created_at: new Date(),
     status: 1,
-  });
+  };
+
+  tasks.push(newTask);
+  createElementTask(newTask);
   // cuando se termine de crear la tarea
   // 1. Limpio el form
   this.reset();
@@ -56,6 +60,44 @@ function countCreatedTask() {
   });
 
   taskCount.textContent = createdFilter.length;
+}
+
+function createElementTask(newTask) {
+  const html = `<div
+              class="list-group-item list-group-item-action p-0 mb-3 border rounded-3 shadow-sm"
+            >
+              <div class="d-flex p-3">
+                <div class="form-check me-2">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    value=""
+                    id="task1"
+                  />
+                </div>
+                <div class="flex-grow-1">
+                  <h5 class="mb-1 fw-semibold">${newTask.task}</h5>
+                  <small class="text-muted">
+                    <i class="bi bi-calendar3 me-1"></i> ${newTask.created_at}
+                  </small>
+                </div>
+              </div>
+              <div
+                class="card-footer bg-light p-2 d-flex justify-content-end border-top"
+              >
+                <button
+                  type="button"
+                  class="btn btn-outline-primary btn-sm me-2"
+                >
+                  <i class="bi bi-pencil"></i> Editar
+                </button>
+                <button type="button" class="btn btn-outline-danger btn-sm">
+                  <i class="bi bi-trash"></i> Eliminar
+                </button>
+              </div>
+            </div> `;
+  // innerHTML: Es el que permite agregar un texto como html
+  taskList.innerHTML += html;
 }
 
 countCreatedTask();
