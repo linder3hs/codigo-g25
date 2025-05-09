ea4d2408e4ccf681170434283461309fa9180d8e
linder3hs <linderhassinger00@gmail.com>
2025-05-08 22:08:27 -0500

    pokemons

--- react-semana5/src/App.jsx
+++ react-semana5/src/App.jsx
@@ -37,9 +37,19 @@ export default function App() {
         </h1>
       </section>
       <section className="pokemons-container">
-        {pokemons.map((pokemon) => (
-          <div className="border border-gray-300 p-5 rounded-md">
-            <h3 className="text-center capitalize text-lg font-semibold">
+        {pokemons.map((pokemon, index) => (
+          <div
+            key={index}
+            className="border border-gray-300 p-5 rounded-md flex flex-col items-center"
+          >
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+                index + 1
+              }.svg`}
+              alt={pokemon.name}
+              width={200}
+            />
+            <h3 className="text-center capitalize text-xl font-semibold">
               {pokemon.name}
             </h3>
           </div>
