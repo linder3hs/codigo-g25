f55c47341317f1fa259e62ab01a26922e64559c2
linder3hs <linderhassinger00@gmail.com>
2025-05-08 22:32:10 -0500

    react router dom

--- react-semana5/src/App.jsx
+++ react-semana5/src/App.jsx
@@ -1,60 +1,65 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// export default function App() {
+//   const [pokemons, setPokemons] = useState([]);
+
+//   const getPokemons = async () => {
+//     try {
+//       const url = "https://pokeapi.co/api/v2/pokemon/";
+
+//       const response = await fetch(url);
+
+//       if (response.ok === false) {
+//         // forzar el llamado al error
+//         throw new Error("Error del Servidor");
+//       }
+
+//       const data = await response.json();
+
+//       console.log(data);
+//       setPokemons(data.results);
+//     } catch (error) {
+//       console.log(error);
+//     }
+
+//     // seguimos con el codigo
+//   };
+
+//   useEffect(() => {
+//     getPokemons();
+//   }, []);
+
+//   return (
+//     <main>
+//       <section className="text-center">
+//         <h1 className="text-red-500 xl:text-green-500 text-3xl my-5">
+//           PokeApi
+//         </h1>
+//       </section>
+//       <section className="pokemons-container">
+//         {pokemons.map((pokemon, index) => (
+//           <div
+//             key={index}
+//             className="border border-gray-300 p-5 rounded-md flex flex-col items-center"
+//           >
+//             <img
+//               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+//                 index + 1
+//               }.svg`}
+//               alt={pokemon.name}
+//               width={200}
+//             />
+//             <h3 className="text-center capitalize text-xl font-semibold">
+//               {pokemon.name}
+//             </h3>
+//           </div>
+//         ))}
+//       </section>
+//     </main>
+//   );
+// }
+import Router from "./router/router";
 
 export default function App() {
-  const [pokemons, setPokemons] = useState([]);
-
-  const getPokemons = async () => {
-    try {
-      const url = "https://pokeapi.co/api/v2/pokemon/";
-
-      const response = await fetch(url);
-
-      if (response.ok === false) {
-        // forzar el llamado al error
-        throw new Error("Error del Servidor");
-      }
-
-      const data = await response.json();
-
-      console.log(data);
-      setPokemons(data.results);
-    } catch (error) {
-      console.log(error);
-    }
-
-    // seguimos con el codigo
-  };
-
-  useEffect(() => {
-    getPokemons();
-  }, []);
-
-  return (
-    <main>
-      <section className="text-center">
-        <h1 className="text-red-500 xl:text-green-500 text-3xl my-5">
-          PokeApi
-        </h1>
-      </section>
-      <section className="pokemons-container">
-        {pokemons.map((pokemon, index) => (
-          <div
-            key={index}
-            className="border border-gray-300 p-5 rounded-md flex flex-col items-center"
-          >
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-                index + 1
-              }.svg`}
-              alt={pokemon.name}
-              width={200}
-            />
-            <h3 className="text-center capitalize text-xl font-semibold">
-              {pokemon.name}
-            </h3>
-          </div>
-        ))}
-      </section>
-    </main>
-  );
+  return <Router />;
 }
