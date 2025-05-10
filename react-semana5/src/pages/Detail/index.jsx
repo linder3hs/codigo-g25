d28ab916262355674d0127ded7a945ded48faa38
linder3hs <linderhassinger00@gmail.com>
2025-05-09 22:35:57 -0500

    new assets

--- react-semana5/src/pages/Detail/index.jsx
+++ react-semana5/src/pages/Detail/index.jsx
@@ -30,7 +30,7 @@ export default function Detail() {
   return (
     <section className="bg-orange-400 min-h-screen">
       {pokemon ? (
-        <>
+        <div className="max-w-lg mx-auto">
           <div className="text-white flex items-center justify-between p-5">
             <div className="flex gap-5">
               <img width={32} src={arrowBack} alt="" />
@@ -41,7 +41,7 @@ export default function Detail() {
             </div>
           </div>
           <div className="mx-2 bg-white p-5 rounded-xl relative mt-40">
-            <div className="flex justify-center max-h-[200px] mt-8 absolute -top-48 w-full">
+            <div className="flex justify-center max-h-[200px] right-0 left-0 mt-8 absolute -top-48 w-full">
               <img
                 src={pokemon.sprites.other.dream_world.front_default}
                 alt={pokemon.name}
@@ -50,15 +50,33 @@ export default function Detail() {
               />
             </div>
             <div className="mt-10">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius
-                quidem eos enim architecto cupiditate in quo alias aspernatur
-                cum! Obcaecati atque officia aliquam nemo delectus quis eaque
-                consectetur. Corrupti, esse.
-              </p>
+              <div className="flex justify-center gap-5">
+                {pokemon.types.map((item, index) => (
+                  <p
+                    key={index}
+                    className="bg-orange-400 py-1 px-2 text-white font-semibold capitalize rounded-xl"
+                  >
+                    {item.type.name}
+                  </p>
+                ))}
+              </div>
+              <section className="mt-8">
+                <h4 className="text-center text-orange-400 font-bold">About</h4>
+                <section className="mt-4 flex justify-between text-center">
+                  <div className="flex-1">
+                    <p>Weight</p>
+                  </div>
+                  <div className="border-x flex-1 border-gray-300">
+                    <p>Height</p>
+                  </div>
+                  <div className="flex-1">
+                    <p>Moves</p>
+                  </div>
+                </section>
+              </section>
             </div>
           </div>
-        </>
+        </div>
       ) : (
         <div>
           <p>Loading...</p>
