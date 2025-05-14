d015887d9319c463a0c9a289b941660f7cea05cf
linder3hs <linderhassinger00@gmail.com>
2025-05-13 20:19:45 -0500

    react components

---
+++ react-semana5/src/components/PokemonList.jsx
@@ -0,0 +1,29 @@
+import usePokemon from "../hooks/usePokemon";
+import { Link } from "react-router-dom";
+
+export default function PokemonList() {
+  const { pokemons } = usePokemon();
+
+  return (
+    <section className="pokemons-container">
+      {pokemons.map((pokemon, index) => (
+        <Link
+          to={`/pokemon/${pokemon.name}`}
+          key={index}
+          className="border border-gray-300 p-5 rounded-md flex flex-col items-center"
+        >
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+              index + 1
+            }.svg`}
+            alt={pokemon.name}
+            width={200}
+          />
+          <h3 className="text-center capitalize text-xl font-semibold">
+            {pokemon.name}
+          </h3>
+        </Link>
+      ))}
+    </section>
+  );
+}
--- react-semana5/src/pages/Home/index.jsx
+++ react-semana5/src/pages/Home/index.jsx
@@ -1,9 +1,6 @@
-import usePokemon from "../../hooks/usePokemon";
-import { Link } from "react-router-dom";
+import PokemonList from "../../components/PokemonList";
 
 export default function Home() {
-  const { pokemons } = usePokemon();
-
   return (
     <main>
       <section className="text-center">
@@ -11,26 +8,7 @@ export default function Home() {
           PokeApi
         </h1>
       </section>
-      <section className="pokemons-container">
-        {pokemons.map((pokemon, index) => (
-          <Link
-            to={`/pokemon/${pokemon.name}`}
-            key={index}
-            className="border border-gray-300 p-5 rounded-md flex flex-col items-center"
-          >
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
-                index + 1
-              }.svg`}
-              alt={pokemon.name}
-              width={200}
-            />
-            <h3 className="text-center capitalize text-xl font-semibold">
-              {pokemon.name}
-            </h3>
-          </Link>
-        ))}
-      </section>
+      <PokemonList />
     </main>
   );
 }
