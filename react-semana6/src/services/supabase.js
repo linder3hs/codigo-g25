e989d964a4a336b0f67a2299407b25dfa951add7
linder3hs <linderhassinger00@gmail.com>
2025-05-16 19:48:24 -0500

    supabase auth

---
+++ react-semana6/src/services/supabase.js
@@ -0,0 +1,29 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
+
+// en supabase por defecto solo necesita el email, password
+// si queremos insertar información extra del usuario se llama metada
+export async function signUpWithEmail(email, password, metadata) {
+  try {
+    // la respuesta que nos da supabase es un objeto que contiene lo siguoente
+    /**
+     {
+      data: {},
+      error: {}
+     }
+     */
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: metadata,
+    });
+
+    console.log({ data, error });
+  } catch (error) {
+    console.log(error);
+  }
+}
