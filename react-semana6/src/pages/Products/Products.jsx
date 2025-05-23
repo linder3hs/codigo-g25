d0e34fa9ca9b8f9692980a7362c33c35cbba0e48
linder3hs <linderhassinger00@gmail.com>
2025-05-22 19:59:29 -0500

    data from form

--- react-semana6/src/pages/Products/Products.jsx
+++ react-semana6/src/pages/Products/Products.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -11,6 +12,32 @@ import {
 import { Input } from "@/components/ui/input";
 
 export function Products() {
+  const [values, setValues] = useState({
+    name: "",
+    description: "",
+    price: 0,
+    brand: "",
+    category: "",
+    image: null,
+  });
+
+  const handleInputChange = (event) => {
+    // ... -> spread operator (crea una copia del objeto)
+    // event.target.name -> obtiene el valor del atributo name
+    // event.target.value -> obtiene el valor que el usuario esta escribiendo
+    const name = event.target.name;
+    setValues({
+      ...values,
+      [name]: name === "image" ? event.target.files[0] : event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(values);
+  };
+
   return (
     <section>
       <div>
@@ -24,17 +51,57 @@ export function Products() {
               <DialogHeader>
                 <DialogTitle>Crear Productos</DialogTitle>
               </DialogHeader>
-              <div className="grid grid-cols-2 gap-5">
-                <div>
-                  <Input placeholder="Nombre del producto" />
+              <form onSubmit={handleSubmit}>
+                <div className="grid grid-cols-2 gap-5 mt-4">
+                  <div>
+                    <Input
+                      name="name"
+                      placeholder="Nombre del producto"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      name="description"
+                      placeholder="Descripción del producto"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      name="price"
+                      placeholder="Precio"
+                      type="number"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      name="brand"
+                      placeholder="Marca"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      name="category"
+                      placeholder="Categoria"
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <Input
+                      name="image"
+                      placeholder="Imagen"
+                      type="file"
+                      onChange={handleInputChange}
+                    />
+                  </div>
                 </div>
-                <div>
-                  <Input placeholder="Descripción del producto" />
+                <div className="mt-5">
+                  <Button className="w-full">Enviar</Button>
                 </div>
-                <div>
-                  <Input placeholder="Precio" type="number" />
-                </div>
-              </div>
+              </form>
             </DialogContent>
           </Dialog>
         </div>
